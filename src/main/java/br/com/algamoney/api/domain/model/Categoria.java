11a95c04589677348fb19685a6529f184454296b
Gilson Silva <gilsonsilvati@gmail.com>
2020-09-08 16:36:02 -0300

    Validando valores inválidos com Bean Validation

--- src/main/java/br/com/algamoney/api/domain/model/Categoria.java
+++ src/main/java/br/com/algamoney/api/domain/model/Categoria.java
@@ -6,12 +6,16 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
 @Getter @Setter
 public class Categoria extends EntidadeBase {
 
+    @NotNull
+    @Size(min = 3, max = 50)
     private String nome;
 
 }
--- src/main/java/br/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/br/com/algamoney/api/resource/CategoriaResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -27,7 +28,7 @@ public class CategoriaResource {
     }
 
     @PostMapping
-    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+    public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria) {
         Categoria categoriaSalva = categorias.save(categoria);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
