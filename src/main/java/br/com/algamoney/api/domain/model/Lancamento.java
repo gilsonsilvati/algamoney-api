9fa30b43c800c3c876bc87959713ecc2909b40ce
Gilson Silva <gilsonsilvati@gmail.com>
2020-10-29 14:53:18 -0300

    Validando lançamento com Bean Validation

--- src/main/java/br/com/algamoney/api/domain/model/Lancamento.java
+++ src/main/java/br/com/algamoney/api/domain/model/Lancamento.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -13,24 +14,31 @@ import java.time.LocalDate;
 @Getter @Setter
 public class Lancamento extends EntidadeBase {
 
+    @NotNull
     private String descricao;
 
+    @NotNull
     @Column(name = "data_vencimento")
     private LocalDate dataVencimento;
 
     @Column(name = "data_pagamento")
     private LocalDate dataPagamento;
 
+    @NotNull
     private BigDecimal valor;
+
     private String observacao;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private TipoLancamento tipo;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "codigo_categoria")
     private Categoria categoria;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "codigo_pessoa")
     private Pessoa pessoa;
