0da11119146d28a85666dfcd5ce99bc639687b8c
Gilson Silva <gilsonsilvati@gmail.com>
2020-10-29 12:26:48 -0300

    Desafio: Cadastrando o primeiro lançamento

--- src/main/java/br/com/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/br/com/algamoney/api/resource/LancamentoResource.java
@@ -2,13 +2,15 @@ package br.com.algamoney.api.resource;
 
 import br.com.algamoney.api.domain.model.Lancamento;
 import br.com.algamoney.api.domain.repository.Lancamentos;
+import br.com.algamoney.api.event.RecursoCriadoEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,6 +19,9 @@ public class LancamentoResource {
 
     @Autowired
     private Lancamentos lancamentos;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @GetMapping
     public List<Lancamento> listar() {
@@ -28,6 +33,14 @@ public class LancamentoResource {
         return lancamentos.findById(codigo)
                 .map(lancamento -> ResponseEntity.ok(lancamento))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentos.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamento);
     }
 
 }
